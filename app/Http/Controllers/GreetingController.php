2792f36784304f75d41a5fced6cff6748b44b1cf
yuta051214 <ffftrick123@gmail.com>
2022-09-10 08:49:44 +0900

    Controllerのインデント修正

--- app/Http/Controllers/GreetingController.php
+++ app/Http/Controllers/GreetingController.php
@@ -6,35 +6,38 @@ use Illuminate\Http\Request;
 
 class GreetingController extends Controller
 {
-    public function greetings($time){
-        switch($time){
-        case "morning":
-            $time = "朝";
-            $greet = "おはようございます";
-            break;
-        case "afternoon":
-            $time = "昼";
-            $greet = "こんにちは";
-            break;
-        case "evening":
-            $time = "夕方";
-            $greet = "こんばんは";
-            break;
-        case "night":
-            $time = "夜";
-            $greet = "おやすみ";
-            break;
+    public function greetings($time)
+    {
+        switch ($time) {
+            case "morning":
+                $time = "朝";
+                $greet = "おはようございます";
+                break;
+            case "afternoon":
+                $time = "昼";
+                $greet = "こんにちは";
+                break;
+            case "evening":
+                $time = "夕方";
+                $greet = "こんばんは";
+                break;
+            case "night":
+                $time = "夜";
+                $greet = "おやすみ";
+                break;
         }
         return view("comments.greetings", ["time" => $time, "greet" => $greet]);
     }
 
-    public function freewords($word){
-    return view("comments.freewords", ["word" => $word]);
+    public function freewords($word)
+    {
+        return view("comments.freewords", ["word" => $word]);
     }
 
-    public function random(){
-    $greets = ["おはよう", "こんにちは", "こんばんは", "おやすみ"];
-    $greet = $greets[array_rand($greets, 1)];
-    return view("comments.random", ["greet" => $greet]);
+    public function random()
+    {
+        $greets = ["おはよう", "こんにちは", "こんばんは", "おやすみ"];
+        $greet = $greets[array_rand($greets, 1)];
+        return view("comments.random", ["greet" => $greet]);
     }
 }
